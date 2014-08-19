9a9359b02b308d9bcad57030b678b96206c7a4a7
ye.jia.alice <ye.jia.alice@gmail.com>
2014-08-19 17:24:45 +0800

    Modify the 'wp-config.php' for DEBUG usage.

--- wp-config.php
+++ wp-config.php
@@ -78,18 +78,19 @@ define('WPLANG', 'zh_CN');
  * 将这个值改为true，WordPress将显示所有用于开发的提示。
  * 强烈建议插件开发者在开发环境中启用WP_DEBUG。
  */
-define('WP_DEBUG', false);
-
+//define('WP_DEBUG', false);
+define('WP_DEBUG', true); //20140819
 /**
  * zh_CN本地化设置：启用ICP备案号显示
  *
  * 可在设置→常规中修改。
  * 如需禁用，请移除或注释掉本行。
  */
-define('WP_ZH_CN_ICP_NUM', true);
+//define('WP_ZH_CN_ICP_NUM', true);
+define('WP_ZH_CN_ICP_NUM', false); //20140819
 
 /* 好了！请不要再继续编辑。请保存本文件。使用愉快！ */
-
+echo("<script>console.log('".dirname(__FILE__)."');</script>"); //20140819, just output the dirname(__FILE__) to browser console
 /** WordPress目录的绝对路径。 */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
